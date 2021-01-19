874ed68092539137b2644a1c913b97a861a88675
root <lucyna.petitpas@gmail.com>
2021-01-19 18:18:18 +0100

    =fix

---
+++ Client/src/components/switch/Switch.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import FormGroup from "@material-ui/core/FormGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import SwitchButton from "@material-ui/core/Switch";
+import {withStyles} from "@material-ui/core";
+import style from "./SwitchStyle";
+
+const Switch = (props) => {
+    const {classes, checked, onChange, label} = props;
+
+    return (
+        <FormGroup>
+            <FormControlLabel
+                control={
+                    <SwitchButton
+                        focusVisibleClassName={classes.focusVisible}
+                        disableRipple
+                        classes={{
+                            root: classes.root,
+                            switchBase: classes.switchBase,
+                            thumb: classes.thumb,
+                            track: classes.track,
+                            checked: classes.checked,
+                        }}
+                        checked={checked}
+                        onChange={onChange}
+                        {...props}
+                    />
+                }
+                label={label}
+            />
+        </FormGroup>
+    );
+};
+
+export default withStyles(style)(Switch);
---
+++ Domoticz/src/schema/models/weather/index.js
@@ -0,0 +1,9 @@
+import WeatherType from "./type/weather";
+
+export const DBWeather = "Weather";
+
+export default {
+    type: [
+        WeatherType,
+    ],
+};
