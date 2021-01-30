81afc330e3de2a5275018d76bcb9a00cec99f6ab
Samlek989 <lucyna.petitpas@gmail.com>
2021-01-30 17:25:35 +0100

    update rooter

--- Client/src/context/admin/config/router.js
+++ Client/src/context/admin/config/router.js
@@ -1,6 +1,9 @@
 import {lazy} from "react";
 
 const User = lazy(() => import("../users/Users"));
+const UserProfile = lazy(() => import("../userProfile/UserProfile"));
+const UserPermissions = lazy(() => import("../userPermissions/UserPermissions"));
+const DeviceProfile = lazy(() => import("../deviceProfile/DeviceProfile"));
 
 export const defaultRoute = "/admin/users";
 export const name = "User";
@@ -11,6 +14,30 @@ const dashboardRoutes = [
         path: "/admin/users",
         name: "Users",
         component: User,
+        exact: true,
+    },
+    {
+        path: "/admin/user/:userId",
+        name: "UserProfile",
+        component: UserProfile,
+        exact: true,
+    },
+    {
+        path: "/admin/user/:userId/permissions",
+        name: "UserPermissions",
+        component: UserPermissions,
+        exact: true,
+    },
+    {
+        path: "/admin/device/:userId",
+        name: "DeviceProfile",
+        component: DeviceProfile,
+        exact: true,
+    },
+    {
+        path: "/admin/device/:userId/scenes",
+        name: "UserPermissions",
+        component: UserPermissions,
         exact: true,
     },
 ];
