24c0888a6338234b8cd375e21220a4d643ce9c27
Marlair Bertrand <4173378-BertrandMarlair@users.noreply.gitlab.com>
2021-01-31 23:28:02 +0100

    new build front

--- Setup/build.sh
+++ Setup/build.sh
@@ -8,6 +8,3 @@ cd Client && npm run build && cd ..
 cd Config && npm run build && cd ..
 cd Gateway && npm run build && cd ..
 cd Domotics && npm run build && cd ..
-
-sudo docker-compose -f docker-compose.build.yml build
-sudo docker-compose -f docker-compose.build.yml push
