11804162b27588d31e1a7189bb194b7c5d0f0001
Marlair Bertrand <4173378-BertrandMarlair@users.noreply.gitlab.com>
2021-02-01 00:40:43 +0100

    update setup

--- Setup/start.sh
+++ Setup/start.sh
@@ -7,7 +7,7 @@ cd Auth/dist && pm2 start api.bundle.js && cd ../..
 cd Config/dist && pm2 start api.bundle.js && cd ../..
 cd Gateway/dist && pm2 start api.bundle.js && cd ../..
 cd Domoticz/dist && pm2 start api.bundle.js && cd ../..
-cd Client && pm2 serve build -s -l 3000 --spa && cd ../..
+cd Client && pm2 serve build -s -l 3000 --spa && cd ..
 cd MongoAuth && pm2 start "mongod -dbpath ./db --port 27018" && cd ..
 cd MongoDomoticz && pm2 start "mongod -dbpath ./db --port 27017" && cd ..
 cd Setup
