b963713cd472e0d5244cf38aa7d84b7167c50b97
Marlair Bertrand <4173378-BertrandMarlair@users.noreply.gitlab.com>
2021-02-01 15:41:45 +0100

    fix docker-compose command

--- Setup/start.sh
+++ Setup/start.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-docker-compose up --build -d
+sudo docker-compose up --build -d
 cd ../
 cd Auth && pm2 start ecosystem.config.js && cd ..
 cd Config && pm2 start ecosystem.config.js && cd ..
