0b2a815efdb309d798cf28a9211701ad7b023781
Marlair Bertrand <4173378-BertrandMarlair@users.noreply.gitlab.com>
2021-02-01 12:26:08 +0100

    new build

---
+++ Client/ecosystem.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    apps: [
+        {
+            name: "Client",
+            script: "serve build -s -l 3000 --spa",
+        },
+    ],
+};
--- Setup/start.sh
+++ Setup/start.sh
@@ -5,9 +5,9 @@ set -e
 cd ../
 cd MongoDomoticz && pm2 start "mongod -dbpath ./db --port 27019" && cd ..
 cd MongoAuth && pm2 start "mongod -dbpath ./db --port 27018" && cd ..
-cd Auth/dist && pm2 start api.bundle.js && cd ../..
-cd Config/dist && pm2 start api.bundle.js && cd ../..
-cd Gateway/dist && pm2 start api.bundle.js && cd ../..
-cd Domoticz/dist && pm2 start api.bundle.js && cd ../..
-cd Client && pm2 serve build -s -l 3000 --spa && cd ..
+cd Auth && pm2 start ecosystem.config.js && cd ../..
+cd Config && pm2 start ecosystem.config.js && cd ../..
+cd Gateway && pm2 start ecosystem.config.js && cd ../..
+cd Domoticz && pm2 start ecosystem.config.js && cd ../..
+cd Client && pm2 start ecosystem.config.js && cd ..
 cd Setup
